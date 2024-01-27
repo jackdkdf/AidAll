7255b9e53f36603893472a9c2b3f19fe8808421c
jackdkdf <jack@rockyroost.com>
2024-01-27 17:27:35 -0500

    Added a createview

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import PostingListView
+from .views import PostingListView, PostingCreateView
 
 urlpatterns = [
     path("posting/list", PostingListView.as_view(), name="posting-list"),
+    path("posting/create", PostingCreateView.as_view(), name="posting-create"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
