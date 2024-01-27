7519c4d7f5a97cb8510f8310df4c9afbda0cc5a1
jackdkdf <jack@rockyroost.com>
2024-01-27 15:24:21 -0500

    Added image folder and views

--- core/views.py
+++ core/views.py
@@ -1,8 +1,53 @@
-from django.shortcuts import render
+from django.db.models.query import QuerySet
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.conf import settings
 
-# Required Log In if not logedin  
-class RecipeListView(LoginRequiredMixin, ListView):
-    model = Recipe
+from .models import Posting
+
+# Redirect to login page if not logged in
+# View for handling all volunteer postings
+class PostingListView(LoginRequiredMixin, ListView):
+    model = Posting
     paginate_by = 15
+    template_name = "/pages/posting_list.html"
+
+# Listview with queryset filtered by user's search
+class PostingSearchView(LoginRequiredMixin, ListView):
+    model = Posting
+    paginate_by = 15
+    template_name = "/core/recipe_list.html"
+
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        object_list = Posting.objects.filter(name__icontains=query)
+        return object_list
+
+class RecipeDetailView(DetailView):
+    model = Posting
+
+# TODO
+class RecipeOfTheDayView(DetailView):
+    model = Posting
+
+class RecipeCreateView(CreateView):
+    model = Posting
+    fields = "__all__"
+    success_url = reverse_lazy('recipe-list')
+
+class RecipeUpdateView(UpdateView):
+    model = Posting
+    fields = "__all__"
+
+class RecipeDeleteView(DeleteView):
+    model = Recipe
+    success_url = reverse_lazy('recipe-list')
+
+class RecipeSavedView(ListView):
+    model = Recipe
+    template_name = "/core/recipe_list.html"
+
+class RecipeCartView(ListView):
+    model = Recipe
     template_name = "/core/recipe_list.html"
