d47de55b19d01dcb486d5a7102ef67c4ec1a0ddd
jackdkdf <jack@rockyroost.com>
2024-01-27 17:45:00 -0500

    Added detailed view

--- core/urls.py
+++ core/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import PostingListView, PostingCreateView
+from .views import PostingListView, PostingCreateView, PostingDetailView
 
 urlpatterns = [
     path("posting/list", PostingListView.as_view(), name="posting-list"),
     path("posting/create", PostingCreateView.as_view(), name="posting-create"),
+    path("posting/<int:pk>/details", PostingDetailView.as_view(), name="posting-detail"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
